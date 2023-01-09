d1b59bd61f1919dca177982ca5ba6847a32bf008
br4d327 <kravcov1640@gmail.com>
2023-01-09 14:45:16 +0500

    init
    add main.py and requirements

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from lyricsgenius import Genius
+from transformers import pipeline
+
+genius = Genius(token)
+
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
+
+def predict(text):
+    return pipe(text)[0]["translation_text"]
+
+
+def get_lyrics(song_name):
+    song = genius.search_song(song_name)
+    lyrics = song.lyrics[song.lyrics.find('Lyrics')+6:]
+    lyrics = lyrics.split('\n')
+    res = ''
+    for row in lyrics:
+        st.progress(row)
+        if '[' in row:
+            res += row
+            res += '\n'
+        else:
+            res += predict(row)
+            res += '\n'
+    return res
+    
+result = get_lyrics("Praise the Lord (Da Shine)")
+
+st.title('Перевод песен Genius')
+song_name = st.text_input('Введите название песни с Genius')
+result = st.button('Перевести')
+if result:
+    preds = get_lyrics(song_name)
+    st.write('**Результаты перевода:**')
+    st.write(preds)
