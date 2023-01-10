4677f7d68fb3ce69e759642d497ccdb40cbb4327
br4d327 <kravcov1640@gmail.com>
2023-01-10 22:49:32 +0500

    finally

--- test_good.py
+++ test_good.py
@@ -4,15 +4,15 @@ import pytest
 
 # Тест load_model
 def test_load_model():
-    pipe = good.load_model()
-    assert good.load_model().tokenizer.name_or_path == 'Helsinki-NLP/opus-mt-en-ru', 'Helsinki-NLP/opus-mt-en-ru'
-    assert good.load_model().tokenizer.vocab_size == 62518, '62518'
-    assert good.load_model().tokenizer.model_max_length == 512, '512'
+    pipe = load_model()
+    assert load_model().tokenizer.name_or_path == 'Helsinki-NLP/opus-mt-en-ru', 'Helsinki-NLP/opus-mt-en-ru'
+    assert load_model().tokenizer.vocab_size == 62518, '62518'
+    assert load_model().tokenizer.model_max_length == 512, '512'
 
 # Тест predict
 def test_predict():
     for key in dict_test.keys():
-        assert good.predict(key) == dict_test[key]
+        assert predict(key) == dict_test[key]
 
 
 dict_test = {'Hello': 'Здравствуйте.'
